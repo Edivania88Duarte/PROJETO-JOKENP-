e8b14361a0c0b8eb096daa20820164aaaff32887
Edivania88Duarte <edivania88duarte@gmail.com>
2023-09-10 12:14:05 -0300

    Projeto JokenPô

---
+++ script.js
@@ -0,0 +1,49 @@
+const result = document.querySelector('.result')
+const humanScore = document.querySelector('#human-score')
+const machineScore = document.querySelector('#machine-score')
+
+
+let humanScoreNumber = 0
+let machineScoreNumber = 0
+
+
+const playHuman = (humanChoice) => {
+    //console.log(humanChoice)
+
+    //console.log(playMachine())
+
+    playTheGame(humanChoice, playMachine())
+
+}
+
+const playMachine = () => {
+    const choices = ['rock', 'paper', 'scissors']
+    const randomNumber = Math.floor(Math.random() * 3)
+
+    return choices[randomNumber]
+}
+
+const playTheGame = (human, machine) => {
+
+    console.log('Humano: ' + human + "Maquina: " + machine)
+
+    if (human === machine) {
+        result.innerHTML = "EMPATE!"
+    } else if (
+        (human === 'paper' && machine === 'rock') ||
+        (human === 'rock' && machine === 'scissors') ||
+        (human === 'scissors' && machine === 'paper')
+    ) {
+        humanScoreNumber++
+        humanScore.innerHTML = humanScoreNumber
+        result.innerHTML = "VOCÊ GANHOU!"
+    } else {
+        machineScoreNumber++
+        machineScore.innerHTML = machineScoreNumber
+        result.innerHTML = "ALEXA GANHOU!"
+
+    }
+
+}
+
+
